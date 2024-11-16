fe7078a5b7439063996f2e519e32e9c4f2702bd2
DumbestPerson224 <eTmullall@outlook.com>
2024-11-16 18:00:54 -0500

    Added InteractionManager.js

---
+++ InteractionManager.js
@@ -0,0 +1,40 @@
+class InteractionManager{
+    
+    is_input_empty(input){
+        /** 
+        * @description This method checks whether or not the parameter is empty
+        * @param {any} input 
+        * @method is_input_empty
+        * @returns {boolean} whether or not the parameter is empty
+        **/
+       return input == "";
+    }
+
+    is_string(input){
+        /**
+         * @param input 
+         * @method is_string
+         * @returns {boolean} whether or not the parameter is a string
+        **/
+        return typeof input == "string"; 
+    }
+
+    is_number(input){
+        /**
+         * @description This method checks if the parameter is a number
+         * @param input 
+         * @method is_number
+         * @returns whether or not the parameter is a number
+         */
+        return input == typeof 0;
+    }
+
+    is_input_boolean(input){
+        /** 
+         * @param input 
+         * @method is_input_boolean 
+         * @returns whether or not the parameter is a boolean 
+        **/
+       return typeof input == true;
+    }
+};
